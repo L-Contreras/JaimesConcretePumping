67f8a553b9dee653f5145b881413d02673febd08
louis Contreras <louiscontreras@louiss-MacBook-Air.local>
2025-06-18 16:52:42 -0400

    Initial comit of Website

---
+++ index.js
@@ -0,0 +1,16 @@
+const slides = document.querySelectorAll('.slide');
+let current = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+    });
+}
+
+function nextSlide() {
+    current = (current + 1) % slides.length;
+    showSlide(current);
+}
+
+showSlide(current);
+setInterval(nextSlide, 5000);
